5dd64ec0f29c0233fcc5c5dba40cdebcb64acf42
navruz22 <navruz.ochilov98@mail.ru>
2023-08-09 13:02:16 +0300

    article card

---
+++ frontend/src/components/ArticleCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col">
+        <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef"></text></svg>
+            <div class="card-body">
+                <i class="fw-bold">{{ article.title }}</i>
+                <p class="card-text">{{ article.body }}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-body-secondary">9 mins</small>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'article-card',
+        props: {
+            article: { 
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,22 +1,31 @@
 <template>
     <div>
-        <p>Home Page</p>
-        {{ data }}
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <article-card 
+                v-for="article in data" 
+                :key="article.id" 
+                v-bind:article="article"    
+            />
+        </div>
     </div>
 </template>
 
 <script>
     import { mapState } from 'vuex';
+    import ArticleCard from "@/components/ArticleCard.vue"
     export default {
         computed: {
             ...mapState({
-                data: state => state.articles,
+                data: state => state.article.data,
                 isLoading: state => state.articles.isLoading,
                 error: state => state.articles.error
             })
         },
         mounted() {
             this.$store.dispatch('articles')
+        },
+        components: {
+            ArticleCard
         }
     }
 </script>
